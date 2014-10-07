d246485ae8beff762d417f168e746e51ed8cc1ac
Ellison Graham <elizabeth.ellison@gmail.com>
2014-10-07 15:30:54 -0400

    second commit

--- UITableView Controller Challenge/EGTableViewController.h
+++ UITableView Controller Challenge/EGTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  EGTableViewController.h
 //  UITableView Controller Challenge
 //
 //  Created by Ellison Graham on 10/7/14.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface EGTableViewController : UITableViewController
 
 @end
-
